669cf542f98be340b66a97656e17c72301a39a0a
Nelloy <nelloy93@yandex.ru>
2020-12-09 11:38:25 +0300

    wip

--- src/FrameExt/Frame/Simple/String.ts
+++ src/FrameExt/Frame/Simple/String.ts
@@ -1,6 +1,9 @@
 import { Log } from '../../../Utils'
 import { Frame } from "../../Frame";
 
+type VertAlignment = 'TOP'|'MID'|'BOT'
+type HorzAlignment = 'LEFT'|'CENTER'|'RIGHT'
+
 export class SimpleString extends Frame {
     constructor(handle: jframehandle){
         super(handle, true)
@@ -34,7 +37,39 @@ export class SimpleString extends Frame {
         BlzFrameSetFont(this.handle, this._font, this._font_size, this._font_flags)
     }
 
-    set textAlignment(a: number){BlzFrameSetTextAlignment(this.handle, TEXT_ALI)}
+    get textVertAlignment(): VertAlignment{
+        switch (this._ver_align){
+            case TEXT_JUSTIFY_TOP: {return 'TOP'}
+            case TEXT_JUSTIFY_MIDDLE: {return 'MID'}
+            case TEXT_JUSTIFY_BOTTOM: {return 'BOT'}
+            default: {return 'MID'}
+        }
+    }
+    set textVertAlignment(align: VertAlignment){
+        switch (align){
+            case 'TOP': {this._ver_align = TEXT_JUSTIFY_TOP}
+            case 'MID': {this._ver_align = TEXT_JUSTIFY_MIDDLE}
+            case 'BOT': {this._ver_align = TEXT_JUSTIFY_BOTTOM}
+        }
+        BlzFrameSetTextAlignment(this.handle, this._ver_align, this._hor_align)
+    }
+
+    get textHorzAlignment(): HorzAlignment{
+        switch (this._hor_align){
+            case TEXT_JUSTIFY_LEFT: {return 'LEFT'}
+            case TEXT_JUSTIFY_CENTER: {return 'CENTER'}
+            case TEXT_JUSTIFY_RIGHT: {return 'RIGHT'}
+            default: {return 'CENTER'}
+        }
+    }
+    set textHorzAlignment(align: HorzAlignment){
+        switch (align){
+            case 'LEFT': {this._hor_align = TEXT_JUSTIFY_LEFT}
+            case 'CENTER': {this._hor_align = TEXT_JUSTIFY_CENTER}
+            case 'RIGHT': {this._hor_align = TEXT_JUSTIFY_RIGHT}
+        }
+        BlzFrameSetTextAlignment(this.handle, this._ver_align, this._hor_align)
+    }
 
     addAction(){
         return Log.err(SimpleString.name + 
@@ -45,8 +80,10 @@ export class SimpleString extends Frame {
                        ': events are not available.')
     }
 
-    private _text: string;
-    private _font: string;
-    private _font_size: number;
-    private _font_flags: number;
+    private _text: string = '';
+    private _font: string = '';
+    private _font_size: number = 10;
+    private _font_flags: number = 0;
+    private _ver_align: jtextaligntype = TEXT_JUSTIFY_MIDDLE;
+    private _hor_align: jtextaligntype = TEXT_JUSTIFY_CENTER;
 }
